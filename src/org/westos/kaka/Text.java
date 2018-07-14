6ab93c304eeaf179b12916d330239c92c436b139
master <123454678@163.com>
2018-07-14 15:01:03 +0800

    11

--- src/org/westos/kaka/Text.java
+++ src/org/westos/kaka/Text.java
@@ -3,5 +3,7 @@ package org.westos.kaka;
 public class Text {
     public static void main(String[] args) {
         System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
     }
 }
