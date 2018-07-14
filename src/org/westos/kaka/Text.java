1c222f667433bd252968c6463363700666f82a07
master <123454678@163.com>
2018-07-14 15:13:13 +0800

    2

--- src/org/westos/kaka/Text.java
+++ src/org/westos/kaka/Text.java
@@ -4,9 +4,7 @@ public class Text {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("他好");
-        System.out.println("他好");
+
 
     }
 }
